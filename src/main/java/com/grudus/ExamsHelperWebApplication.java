c07cf27bf89729bff29fff5ef71e5bf2ff8f314d
grudus <grudus@protonmail.com>
2016-09-11 14:58:39 +0200

    First commit

---
+++ src/main/java/com/grudus/ExamsHelperWebApplication.java
@@ -0,0 +1,21 @@
+package com.grudus;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ExamsHelperWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsHelperWebApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+			System.err.println("Hello, world!");
+		};
+	}
+}
